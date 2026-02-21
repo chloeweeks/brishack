d08ec37a355a924f4de306a4018218611806d173
Chloe Weeks <bg24494@bristol.ac.uk>
2026-02-21 18:21:01 +0000

    added top trump style card

--- constellocate/app/main/page.tsx
+++ constellocate/app/main/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Button from "../components/ui/Button";
 import Canvas from "../src/components/canvas";
+import Image from "next/image";
 
 
 export default function Home() {
@@ -13,11 +14,48 @@ export default function Home() {
       </div>
 
       <div className="border-l border-white">
-        <h1 className="text-2xl text-center font-semibold tracking-tight text-white">
+        <h1 className="text-2xl text-center font-semibold tracking-tight text-white py-6">
           matches:
+  
         </h1>
+
+        {/* Left column */}
+        <div className="w-80 h-140 bg-white rounded-2xl flex flex-col text-black mx-auto">
+          <h1 className = "text-2xl align-top font-bold text-black py-2 px-2">
+          Star Name: 
+          </h1>
+          <div className="flex items-center justify-center py-2">
+            <Image 
+            src = "/star.png"
+            alt = "star image"
+            width = {240}
+            height = {240}
+            className = "my-2"
+            />
+            </div>
+
+       
+
+           <h2 className = "text-2xl align-top font-bold text-black py-2 px-2">
+          Mass: 
+          </h2>
+
+           <h2 className = "text-2xl align-top font-bold text-black py-2 px-2">
+          Magnitude: 
+          </h2>
+
+           <h2 className = "text-2xl align-top font-bold text-black py-2 px-2">
+          Distance: 
+          </h2>
+
+           <h2 className = "text-2xl align-top font-bold text-black py-2 px-2">
+          Spectral Type: 
+          </h2>
+
+        </div>
       
       </div>
+
 
     </div>
   );
