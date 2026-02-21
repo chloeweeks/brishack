e021980c8f7b7028a7efc7555c95377ca1c8d7a8
Katie Young <cq24834@bristol.ac.uk>
2026-02-21 18:22:25 +0000

    glow to buttons

--- constellocate/app/components/ui/Button.tsx
+++ constellocate/app/components/ui/Button.tsx
@@ -12,7 +12,7 @@ interface ButtonProps {
 export default function Button({
   text,
   type,
-  className = 'font-semibold cursor-pointer whitespace-nowrap flex h-12 w-full items-center justify-center gap-2 rounded-full px-5 text-background transition-colors hover:bg-gray-300 md:w-[158px] bg-white',
+  className = 'font-semibold cursor-pointer whitespace-nowrap flex h-12 w-full items-center justify-center gap-2 rounded-full px-5 text-background transition-colors hover:bg-gray-300 md:w-[158px] bg-white hover:shadow-[0_0_35px_rgba(99,102,241,1)]',
   onClick,
 }: ButtonProps) {
   return (
