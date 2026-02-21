f04b9c51abe859f1e032d2a5ef15bf24bab9de0b
Chloe Weeks <bg24494@bristol.ac.uk>
2026-02-21 16:39:11 +0000

    added buttons and seperated screen into 3 - main screen

--- constellocate/app/main/page.tsx
+++ constellocate/app/main/page.tsx
@@ -1,10 +1,37 @@
+'use client';
+import Button from "../components/ui/Button";
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <p>
-        main page 
-      </p>
+    <div className="grid grid-cols-[70%_30%] h-screen bg-black">
+      {/* Left column */}
+      {/* Right column split into 2 rows */}
+      <div className="grid grid-rows-[80%_20%] border-r border-white">
+        <div className="border-b border-white">
+            <h1 className="text-lg text-left font-semibold tracking-tight text-white">
+          Start drawing:
+        </h1>
+          drawing screen
+        </div>
+        <div className = "grid grid-cols-3 w-full gap-4 py-4 px-4">
+         
+           <Button text={'Submit'} onClick={() => (window.location.href = '/load-page')}
+           className="w-full h-full bg-white text-black rounded-lg" />
+           <Button text={'Undo'} 
+           className="w-full h-full bg-white text-black rounded-lg" />
+           <Button text={'Clear'}
+           className="w-full h-full bg-white text-black rounded-lg" />
+           </div>
+      </div>
+
+      <div className="border-l border-white">
+        <h1 className="text-2xl text-center font-semibold tracking-tight text-white">
+          matches:
+        </h1>
+      
+      </div>
+
     </div>
   );
 }
